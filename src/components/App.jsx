24438f4b162f3f6e3389438dfff43b2398f24f02
Thach Nguyen <thachnv45@gmail.com>
2016-06-15 23:05:06 +0700

    Restructuring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import firebaseDemoApp from '../third-party/Firebase.js';
 import GameBoard from './GameBoard/GameBoard.jsx'
 
-function drawCircle() {
-  // console.log(ReactDOM.findDOMNode(this));
-  var canvas = document.getElementById('myCanvas');
-  var context = canvas.getContext('2d');
-  var centerX = canvas.width / 2;
-  var centerY = canvas.height / 2;
-  var radius = 70;
-
-  context.beginPath();
-  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  context.fillStyle = 'green';
-  context.fill();
-  context.lineWidth = 5;
-  context.strokeStyle = '#003300';
-  context.stroke();
-}
-
 export default () => {
-  // firebaseDemoApp.database().ref('players/player0/velocity').on('value', (snapshot)=> {
-  //   console.log(snapshot.val());
-  // });
-
-  // drawCircle();
-
   return (
     <div>
       Welcome!
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import d3 from 'd3';
+import Player from '../Player/Player.js';
 import firebaseDemoApp from '../../third-party/Firebase.js';
 
 const MAX_VELOCITY = 80;
@@ -10,49 +11,92 @@ export default class GameBoard extends React.Component {
   constructor(...args) {
     super(...args);
     this.timer = null;
-    this.velocity = {x: 0, y: 0};
-    this.position = {x: 250, y: 250};
+    this.players = [];
   }
 
-  getNewPosition() {
-    const speed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.y * this.velocity.y);
+  getNewPosition(player) {
+    let position = player.position;
+    let velocity = player.velocity;
+
+    const speed = Math.sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
+    let newX = position.x + speed * velocity.x / MAX_VELOCITY / SPEED_CONST;
+    let newY = position.y + speed * velocity.y / MAX_VELOCITY / SPEED_CONST;
+    if (newX > 500 - player.size * 2 || newX < 0) {
+      newX = position.x;
+    }
+    if (newY > 500 - player.size * 2 || newY < 0) {
+      newY = position.y;
+    }
     return {
-      x: this.position.x + speed * this.velocity.x / MAX_VELOCITY / SPEED_CONST,
-      y: this.position.y + speed * this.velocity.y / MAX_VELOCITY / SPEED_CONST,
+      x: newX,
+      y: newY,
     };
   }
 
+  addPlayer() {
+    //Should listen to new player event
+    let newPlayer = {
+      id: 0,
+      size: 20,
+      position: {x: 250, y: 250},
+      velocity: {x: 0, y: 0},
+    };
+
+    firebaseDemoApp.database().ref('players/player' + newPlayer.id + '/velocity').on('value', (snapshot)=> {
+      newPlayer.velocity = snapshot.val();
+    });
+
+    this.players.push(newPlayer);
+  }
+
   componentDidMount() {
-    this.svg = d3.select(ReactDOM.findDOMNode(this));
-    this.drawCircle();
+    // this.svg = d3.select(ReactDOM.findDOMNode(this));
+    // this.drawCircle();
 
     this.timer = setInterval(() => {
       window.requestAnimationFrame(() => {
-        this.player0
-          .attr('cx', this.position.x)
-          .attr('cy', this.position.y);
+        this.players.forEach((player)=> {
+          player.position = this.getNewPosition(player)
+        });
+        this.forceUpdate();
+        // this.players.forEach((player) => {
+        //   player.position = this.getNewPosition(player.position, player.velocity);
+        //   this.player0
+        //     .attr('cx', player.position.x)
+        //     .attr('cy', player.position.y);
+        // });
 
-        this.position = this.getNewPosition();
       });
     }, 1);
 
-    firebaseDemoApp.database().ref('players/player0/velocity').on('value', (snapshot)=> {
-      this.velocity = snapshot.val();
-    });
+    this.addPlayer();
+    //
+    // firebaseDemoApp.database().ref('players/player0/velocity').on('value', (snapshot)=> {
+    //   this.players[0].velocity = snapshot.val();
+    // let player = {
+    //   verocity: snapshot.val(),
+    // };
+    // this.setState({
+    //   players: [].concat([player])
+    // })
+    // });
   }
 
   drawCircle() {
     this.player0 = this.svg.append('circle')
-      // .attr('cx', 250)
-      // .attr('cy', 250)
       .attr('r', 10)
       .style('fill', 'green')
       .style('transform', 'translate3d(0,0,0)');
   }
 
+
   render() {
     return (
-      <svg width="500" height="500"></svg>
+      <div style={{width: 500, height: 500, position: 'relative', border: '1px solid black'}}>
+        {this.players.map((player) => (
+          <Player key={player.id} radius="20" position={player.position}/>
+        ))}
+      </div>
     )
   }
 }
